f3ed3d38373f2389edc11a91014024343eea2bd7
Yehuda Bortz <yehudabortz@icloud.com>
2020-11-06 10:13:09 -0600

    commit tree 57b31dbf10263df1fd088f9d17413aafdade33b0

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,31 @@
-# Add your code here
+class Dog
+
+    @@all = []
+
+    attr_accessor :name
+
+
+    def initialize(name)
+        @name = name
+        save
+    end
+    
+    def save
+    @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.print_all
+        @@all.each do |dog|
+           puts dog.name
+        end
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+end
